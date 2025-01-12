7d035931f4059bc24c4e46f8ef9d447cf44cc3e1
EfisioCaschili <118173025+EfisioCaschili@users.noreply.github.com>
2025-01-12 19:12:18 +0100

    Update application.py
    Safety email password policy implemented. 
    New customer function implemented.
    More accurate check on token

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import sqlite3
 import mysql.connector
 import secrets
+import hashlib
 try:
     from dotenv import dotenv_values, load_dotenv
 except:
@@ -81,7 +82,32 @@ class Query():
         except Exception as e:
             print(f"Error occurred: {e}")
             return False
+        
+    def get_customer(self, name, surname):
+        try:
+            conn = self.dbConnection(config)
+            if not conn:
+                print("Database connection failed")
+                return False
 
+            # Usa un blocco "with" per gestire il cursore
+            with conn.cursor() as cursor:
+                query = """
+                    SELECT id, gender
+                    FROM customer
+                    WHERE first_name = %s AND last_name = %s
+                """
+                cursor.execute(query, (name,surname))
+                result = cursor.fetchone()
+
+            conn.close()
+            print("Connection to DB closed")
+            if not result:
+                return False
+            return result
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            return False
     
     def get_email_parameters(self,sender_email):
         try:
@@ -92,7 +118,7 @@ class Query():
             
             with conn.cursor() as cursor:
                 # Ottenere il valore corrente del contatore
-                query = "SELECT pssword,smtp_server,smtp_port,id FROM email WHERE mail_address = %s;"
+                query = "SELECT smtp_server,smtp_port,id FROM email WHERE mail_address = %s;"
                 cursor.execute(query, (sender_email,))
                 result = cursor.fetchall()
                 if not result:
@@ -153,11 +179,11 @@ class Query():
             if not table:
                 print("Table name not provided.")
                 return False
-
+            print(table)
             # Rimuovi 'table' dai dati e prepara i campi e i valori
             fields = [key for key in data.keys() if key != 'table']
             values = [data[key] for key in fields]
-
+            print(fields)
             # Costruzione della query con placeholder
             placeholders = ', '.join(['%s'] * len(fields))
             field_names = ', '.join(fields)
@@ -178,27 +204,65 @@ class Query():
                 conn.close()
                 print("Database connection closed.")
 
+@app.route('/add-new-customer', methods=['POST'])
+def add_new_customer():
+    try:
+        data = request.get_json()
+        
+        customer_id = Query().get_customer(data.get('name'),data.get('surname'))
+        print(customer_id)
+        if customer_id:
+            return jsonify({'status': 'error', 'message': 'User already present'}), 400
+
+        Query().create_new_row({'table':'customer',
+                                'last_name':data.get('surname'),
+                                'first_name':data.get('name'),
+                                'gender':data.get('gender')
+                                })
+        
+        customer_id = Query().get_customer(data.get('name'),data.get('surname'))[0]
+        if not customer_id:
+            return jsonify({'status': 'error', 'message': 'Error adding customer'}), 400
+
+        Query().create_new_row({'table':'email',
+                                'mail_address':data.get('mail_address'),
+                                'pssword': data.get('password'),
+                                'smtp_server':data.get('smtp_server'),
+                                'customer_id':customer_id,
+                                'smtp_port':587
+                                })
+        #geneeration of token
+        token = secrets.token_hex(16)
+        user_id=Query().get_email_parameters(data.get('mail_address'))[0][2]
+        Query().create_new_row({'table':'user_token','email_id':user_id,'token':token})
+        return jsonify({'status': 'success', 'message': 'User added in the database'}), 200
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500 
 
 @app.route('/generate-token', methods=['POST'])
 def generate_token(): 
     try:
         data = request.get_json()
         sender_email = data.get('sender_email')
-
+        
         if not sender_email:
-            return jsonify({'status': 'error', 'message': 'Sender email missing'}), 400
+            return jsonify({'status': 'error', 'message': 'Sender email not specified'}), 400
 
+        if not Query().get_email_parameters(sender_email):
+            return jsonify({'status': 'error', 'message': 'Sender email not present in the database'}), 400
         #Token check
         result=Query().check_token(sender_email)
+        print(result)
         if result:
             for x in result:
-                if x[1]== datetime.date.today() and x[2]<50:
+                if x[1]== datetime.date.today() and x[2]<10:
                     return jsonify({'status': 'success', 'message': 'Token recovered and not expired yet', 'token': x}), 200
 
         #New Token Saved
         # Token generation
         token = secrets.token_hex(16)
-        user_id=Query().get_email_parameters(sender_email)[0][3]
+        user_id=Query().get_email_parameters(sender_email)[0][2]
         Query().create_new_row({'table':'user_token','email_id':user_id,'token':token})
         return jsonify({'status': 'success', 'message': 'Token generated successfully', 'token': token}), 201
 
@@ -229,14 +293,16 @@ def send_email():
             if x[1] == datetime.date.today() and x[2] < 50:
                 token_validity=True
                 id=x[3]
+                if token != x[0]:
+                    return jsonify({'status': 'error', 'message': 'Authentication failed. Wrong Token used'}), 401
                 break
         if not token_validity:
             return jsonify({'status': 'error', 'message': 'Authentication failed. Token expired.'}), 401
-        
+         
         parameters=Query().get_email_parameters(sender_email)
-        EMAIL_PASSWORD=parameters[0][0]
-        SMTP_SERVER=parameters[0][1]
-        SMTP_PORT=parameters[0][2]
+        EMAIL_PASSWORD=data.get('password')
+        SMTP_SERVER=parameters[0][0]
+        SMTP_PORT=parameters[0][1]
         recipient = data['recipient']
         subject = data['subject']
         content = data['content']
@@ -254,8 +320,8 @@ def send_email():
             print('Access Server completed')
             try:
                 server.login(sender_email, EMAIL_PASSWORD)
-            except smtplib.SMTPAuthenticationError:
-                print('Login Error')
+            except smtplib.SMTPAuthenticationError as e:
+                print(f'Login Error: {e.smtp_error.decode()}')
                 return jsonify({'status': 'error', 'message': 'Authentication failed. Check email and password.'}), 401
             server.sendmail(sender_email, recipient, msg.as_string())
 
@@ -266,6 +332,5 @@ def send_email():
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
